b31ef6ee5deeeb891dbcc48cb4d01c8a598e7730
Shawn Button <shawn.button@bell.ca>
2019-12-01 12:19:08 -0500

    added generic kata folders

---
+++ test/kata/kata.test.js
@@ -0,0 +1,16 @@
+'use strict'
+/* eslint-env jest */
+
+const sut = require('../../lib/kata/kata')
+
+describe('some set of behaviours', () => {
+  it('should do something', () => {
+    const given = ''
+
+    const expected = ''
+
+    const result = sut.someFunction(given)
+
+    expect(result).toEqual(expected)
+  })
+})
