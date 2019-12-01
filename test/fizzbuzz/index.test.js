f7deb461e049df5f616b26faf9e7b2f62289ad14
Shawn Button <shawn.button@bell.ca>
2019-12-01 12:23:30 -0500

    added fizzbuzz kata skeleton

---
+++ test/fizzbuzz/index.test.js
@@ -0,0 +1,16 @@
+'use strict'
+/* eslint-env jest */
+
+const sut = require('../../lib/fizzbuzz')
+
+describe('fizz buzz game', () => {
+  it('should do something', () => {
+    const given = ''
+
+    const expected = ''
+
+    const result = sut.someFunction(given)
+
+    expect(result).toEqual(expected)
+  })
+})
